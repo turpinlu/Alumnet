bbc2353014ea75c122a4c97e3e328f8b149aaa52
turpinlu <turpinlu@gmail.com>
2015-10-03 16:48:47 -0400

    Moving database connection to new file
    Just use include ‘dataLogin.php’

--- index.php
+++ index.php
@@ -1,25 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <link rel="shortcut icon" href="logo.ico">
+
 <?php
-
-    //address error handling
-    ini_set('display_errors',1); //1 turns it on
-    error_reporting(E_ALL & ~E_NOTICE); //errors_all, except notices (warnings)
-    //Attempt to Connect
-    if ($connection = @mysql_connect ('localhost', 'alumnetx', 'Vu47Z7vis3')){
-    }
-    else {
-        die('<p>Could not connect to MySQL because: <b>'.mysql_error().'</b></p>');
-    }
-    if (@mysql_select_db("alumnetx_ALUMNET", $connection)){
-    }
-    else {
-        die('<p>Could not select the ALUMNET database because:
-            <b>' .mysql_error().'</b></p>');
-    }
+ // MySQL Database Connect
+ include 'datalogin.php';
 ?>
-
 
 <head>
 <title>Alumnet - Networking Made Easy</title>
