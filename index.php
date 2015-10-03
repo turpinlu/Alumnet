7ad246681cf4524d3f9f3549253d5bc7bfb52854
turpinlu <turpinlu@gmail.com>
2015-10-03 17:25:28 -0400

    Oops

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
  $email = $_POST['email'];
  $password = $_POST['password'];
  $password_confirmation = $_POST['password_confirmation'];
- echo "First Name: "
- echo "Last Name: "
- echo "Display Name: "
+ echo "First Name: ";
+ echo "Last Name: ";
+ echo "Display Name: ";
 ?>
 
 <head>
