f72ab5083f72a0adc246c034b5d9468b9506309e
turpinlu <turpinlu@gmail.com>
2015-10-03 17:08:06 -0400

    Adding basic php for registration

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 <?php
  // MySQL Database Connect
  include 'datalogin.php';
+
+ $first_name = $_POST['first_name'];
+ $last_name = $_POST['last_name'];
+ $display_name = $_POST['display_name'];
+ $email = $_POST['email'];
+ $password = $_POST['password'];
+ $password_confirmation = $_POST['password_confirmation'];
 ?>
 
 <head>
@@ -156,34 +163,38 @@
     <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
         <form role="form">
             <h2>Please Sign Up <small>It's free and always will be.</small></h2>
+
+           
+            <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+            
             <hr class="colorgraph">
             <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-6">
                     <div class="form-group">
-                        <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" tabindex="1">
+                        <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" value = "<?echo $first_name ?>" required tabindex="1">
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-6">
                     <div class="form-group">
-                        <input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" tabindex="2">
+                        <input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" value = "<?echo $last_name ?>" required tabindex="2">
                     </div>
                 </div>
             </div>
             <div class="form-group">
-                <input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="Display Name" tabindex="3">
+                <input type="text" name="display_name" id="display_name" class="form-control input-lg" placeholder="Display Name" value = "<?echo $display_name ?>" required tabindex="3">
             </div>
             <div class="form-group">
-                <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="4">
+                <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value = "<?echo $email ?>" required tabindex="4">
             </div>
             <div class="row">
                 <div class="col-xs-12 col-sm-6 col-md-6">
                     <div class="form-group">
-                        <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="5">
+                        <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" required tabindex="5">
                     </div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-6">
                     <div class="form-group">
-                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="6">
+                        <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" required tabindex="6">
                     </div>
                 </div>
             </div>
@@ -191,7 +202,7 @@
                 <div class="col-xs-4 col-sm-3 col-md-3">
                     <span class="button-checkbox">
                         <button type="button" class="btn" data-color="info" tabindex="7"> I Agree</button>
-                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" value="1">
+                        <input type="checkbox" name="t_and_c" id="t_and_c" class="hidden" required value="1">
                     </span>
                 </div>
                 <div class="col-xs-8 col-sm-9 col-md-9">
@@ -202,6 +213,7 @@
             <hr class="colorgraph">
             <div class="row">
                 <div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="7"></div>
+            </form>
                 <div class="col-xs-12 col-md-6"><a href="#" class="btn btn-success btn-block btn-lg">Sign In</a></div>
             </div>
         </form>
