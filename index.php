83493b74a5e6659881a77ef53a200ab6b4d712b6
turpinlu <turpinlu@gmail.com>
2015-10-05 15:50:51 -0400

    FIX: pw confirmation
    cleanStringInput is broken - currently it returns nothing (always)
    the hash encryption is time based, meaning if we hash the pw then pw
    confirmation, and compare them, it’s a different hash.

--- index.php
+++ index.php
@@ -7,16 +7,13 @@
  include 'datalogin.php';
  include 'secure.php';
 
+//cleanStringInput is broken - currently it strips all characters and returns nothing
  $first_name = cleanStringInput($_POST['first_name']);
  $last_name = cleanStringInput($_POST['last_name']);
  $display_name = cleanStringInput($_POST['display_name']);
  $email = cleanStringInput($_POST['email']);
  $password = $_POST['password'];
  $password_confirmation = $_POST['password_confirmation'];
- echo " ";
- echo $password_confirmation;
- echo " ";
- echo $password;
  $pwmatch = strcmp($password_confirmation, $password);
  $password = hashPassword($password);
  $password_confirmation = hashPassword($password_confirmation);
@@ -188,7 +185,7 @@
             </div>
 
             <?php
-                if (!$pwmatch){
+                if ($pwmatch){
             ?>
                 <div class="row">
                     <div class="col-xs-8 col-sm-9 col-md-9">
