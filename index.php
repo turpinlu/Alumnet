af28bdbbd9b723075a3d5e8d858fd39a11ff009c
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-10-06 12:07:37 -0400

    yeah yeah yeah

--- index.php
+++ index.php
@@ -116,17 +116,22 @@
   </div>
   <div class="collapse navbar-collapse">
     <ul class="nav pull-right">
-        <li class="dropdown" id="menuLogin">
-         <a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Login</a>
-         <div class="dropdown-menu" style="padding:17px;">
-             <form class="form" id="formLogin"> 
-             <input name="username" id="username" type="text" placeholder="Username"> 
-             <input name="password" id="password" type="password" placeholder="Password"><br>
-             <button type="button" id="btnLogin" class="btn">Login</button>
-            </form>
-         </div>
-        </li>
-    </ul>
+          <li class="divider-vertical"></li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+            <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+              <!-- Login form here -->
+              <form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
+                  <input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
+                  <input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
+                  <input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
+                  <label class="string optional" for="user_remember_me"> Remember me</label>
+                 
+                  <input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign In" />
+                </form>
+            </div>
+          </li>
+        </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
 
