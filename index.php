80bea2bfab14620bbe434f12ffa31e59e1213b4e
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-10-06 12:09:44 -0400

    um

--- index.php
+++ index.php
@@ -91,6 +91,16 @@
             init();
         });
     });
+
+$(function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+});
 </script>
 <style type="text/css">
     .navbar{
