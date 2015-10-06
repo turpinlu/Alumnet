d257546cd6aa9b306e9d038640215c95b5253bfb
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-10-06 11:20:39 -0400

    alignment

--- index.php
+++ index.php
@@ -146,12 +146,12 @@
 
             <hr class="colorgraph">
             <div class="row">
-                <div class="col-sm-3 col-md-9">
+                <div class="col-xs-3 col-sm-3 col-md-6">
                     <div class="form-group">
                         <input type="text" name="first_name" id="first_name" class="form-control input-lg" placeholder="First Name" value = "<?echo $first_name ?>" required tabindex="1">
                     </div>
                 </div>
-                <div class="col-sm-3 col-md-9">
+                <div class="col-xs-3 col-sm-3 col-md-6">
                     <div class="form-group">
                         <input type="text" name="last_name" id="last_name" class="form-control input-lg" placeholder="Last Name" value = "<?echo $last_name ?>" required tabindex="2">
                     </div>
@@ -164,12 +164,12 @@
                 <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email Address" value = "<?echo $email ?>" required tabindex="4">
             </div>
             <div class="row">
-                <div class="col-sm-3 col-md-9">
+                <div class="col-xs-3 col-sm-3 col-md-6">
                     <div class="form-group">
                         <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" required tabindex="5">
                     </div>
                 </div>
-                <div class="col-sm-3 col-md-9">
+                <div class="col-xs-3 col-sm-3 col-md-6">
                     <div class="form-group">
                         <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" required tabindex="6">
                     </div>
