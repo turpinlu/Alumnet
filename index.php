93e48c79d15922d2026bfb6c6e716bf90f21564c
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-10-06 12:14:01 -0400

    what it is

--- index.php
+++ index.php
@@ -92,14 +92,11 @@
         });
     });
 
-$(function() {
-  // Setup drop down menu
-  $('.dropdown-toggle').dropdown();
- 
-  // Fix input element click problem
-  $('.dropdown input, .dropdown label').click(function(e) {
-    e.stopPropagation();
-  });
+$(document).ready(function(){
+    //Handles menu drop down
+    $('.dropdown-menu').find('form').click(function (e) {
+        e.stopPropagation();
+    });
 });
 </script>
 <style type="text/css">
@@ -125,23 +122,35 @@ $(function() {
     </a>
   </div>
   <div class="collapse navbar-collapse">
-    <ul class="nav pull-right">
-          <li class="divider-vertical"></li>
-          <li class="dropdown">
-            <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
-            <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-              <!-- Login form here -->
-              <form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
-                  <input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
-                  <input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
-                  <input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
-                  <label class="string optional" for="user_remember_me"> Remember me</label>
-                 
-                  <input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign In" />
-                </form>
-            </div>
-          </li>
-        </ul>
+    <ul class="nav navbar-nav navbar-right">
+                  <li class="dropdown">
+                     <a href="http://www.jquery2dotnet.com" class="dropdown-toggle" data-toggle="dropdown">Sign in <b class="caret"></b></a>
+                     <ul class="dropdown-menu" style="padding: 15px;min-width: 250px;">
+                        <li>
+                           <div class="row">
+                              <div class="col-md-12">
+                                 <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                    <div class="form-group">
+                                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                       <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                    </div>
+                                    <div class="form-group">
+                                       <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                       <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                    </div>
+                                    <div class="checkbox">
+                                       <label>
+                                       <input type="checkbox"> Remember me
+                                       </label>
+                                    </div>
+                                    <div class="form-group">
+                                       <button type="submit" class="btn btn-success btn-block">Sign in</button>
+                                    </div>
+                                 </form>
+                              </div>
+                           </div>
+                        </li>
+                     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
 
