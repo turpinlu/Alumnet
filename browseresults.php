580ed61389d9ae8d321ed06ddbded276e50144f7
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-11-04 11:14:54 -0500

    A few design changes

--- browseresults.php
+++ browseresults.php
@@ -19,20 +19,18 @@
 </style>
 </head>
 <body>
-<div class="container">
-    <nav role="navigation" class="navbar navbar-inverse">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="index.php">
-            <img src="logo2.png" style="max-height:50px;max-width:200px;padding-left:10px;">
-            </a>
-        </div>
+<nav class="navbar navbar-custom navbar-static-top">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a href="index.php">
+        <img src="logo2.png" style="max-height:50px;max-width:200px;padding-left:10px;">
+    </a>
+  </div>
         <!-- Collection of nav links, forms, and other content for toggling -->
         <div id="navbarCollapse" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
@@ -40,20 +38,9 @@
                 <li><a href="profile.php">Profile</a></li>
                 <li class="active"><a href="browse.php">Browse</a></li>
                 <li><a href="index.php">Log Out</a></li>
-                <!-- <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Messages<b class="caret"></b></a>
-                    <ul role="menu" class="dropdown-menu">
-                        <li><a href="#">Inbox</a></li>
-                        <li><a href="#">Drafts</a></li>
-                        <li><a href="#">Sent Items</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">Trash</a></li>
-                    </ul>
-                </li> -->
             </ul>
         </div>
     </nav>
-</div>
 <div class="row">
     <div class="container">
         <div class="row-md-6">
