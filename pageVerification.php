9e5cbc5d71b83229c347a4d0b679f9470f13c63c
LUCAS <turpinlu@gmail.com>
2015-11-19 15:56:59 -0500

    asdasd

--- pageVerification.php
+++ pageVerification.php
@@ -1,12 +1,13 @@
 
 <?php
  // MySQL Database Connect
-include 'dataLogin.php';
+//include 'dataLogin.php';
 //include 'myDataLogin.php';
-include 'secure.php';
+//include 'secure.php';
 session_start();
 if (!$_SESSION['email']) {
 	echo " REDIRECTING TO INDEX, NO EMAIL";
+	echo $_SESSION['email'];
 	//die("<script>location.href = 'http://alumnet.xyz/index.php'</script>");
 }
 
