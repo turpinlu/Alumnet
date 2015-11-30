1ab4f758dbff26a34a7c37c59126c029f4169a7c
M. Kroll <mkroll1@gulls.salisbury.edu>
2015-11-30 14:30:08 -0500

    A super catchy slogan

--- index.php
+++ index.php
@@ -316,7 +316,7 @@ $(document).ready(function(){
 </div>
 <div class="col-md-5 col-xl-height col-sm-offset-0"> 
     <div class="jumbotron">
-        <h1>Wow! A profile page!</h1>
+        <h1>Networking Made Easy</h1>
         <p>For those who leave Salisbury, but for whom Salisbury never leaves, this is where you belong: Salisbury University's Alumni Network (Alumnet, for short). Strengthen your Salisbury connection and help support activities of other alumni by becoming a member.</p>
         <p><a href="http://alumnet.xyz" target="_blank" class="btn btn-primary btn-lg">Get started today</a></p>
     </div>
