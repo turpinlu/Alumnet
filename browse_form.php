2cfd513aaf315c28ed00918bee8fe2dde9950f85
LUCAS <turpinlu@gmail.com>
2015-12-07 10:15:21 -0500

    Radio Button

--- browse_form.php
+++ browse_form.php
@@ -53,10 +53,11 @@
 	$query= "SELECT * FROM ACCOUNT, DEGREE, DEGNAME, ACCDEG WHERE ACCDEG.ACCOUNTNUM=ACCOUNT.ACCOUNTNUM AND ACCDEG.DEGNUM=DEGREE.DEGNUM AND DEGREE.DEGREE=DEGNAME.DEGREE $fn $ln $dg $gd $ct $st";
 	
 	if($r1 = mysql_query($query)){
-		echo '<table class="table table-striped table-hover ">';
+		echo '<table class="table table-striped">';
         echo '<thead>';
         echo '<tr class="info">';
        // echo '<tr>';
+         echo '<th>        </th>';
         echo '<th>First Name</th>';
         echo '<th>Last Name</th>';
         echo '<th>Degree</th>';
@@ -74,7 +75,7 @@
 			$grad2 = $row['GRADDATE'];
 			$city2 = $row['CITY'];
 			$state2 = $row['STATE'];         
-			echo '<td><input type="radio" name="radio" id="radio" class="radio" checked/></td>';         
+			echo '<td><input type="radio" name="radio" id="radio" class="radio"/></td>';         
             echo '<td>'.$fname2.'</td>';
             echo '<td>'.$lname2.'</td>';
             echo '<td>'.$degree2.'</td>';
