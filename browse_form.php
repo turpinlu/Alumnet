a1ba09e811a69ae1c0c3794dd5d5c4dd3b0f8537
LUCAS <turpinlu@gmail.com>
2015-12-17 15:20:36 -0500

    Should work

--- browse_form.php
+++ browse_form.php
@@ -58,51 +58,52 @@
 
 
 	$query= "SELECT * FROM ACCOUNT, DEGREE, DEGNAME, ACCDEG WHERE ACCDEG.ACCOUNTNUM=ACCOUNT.ACCOUNTNUM AND ACCDEG.DEGNUM=DEGREE.DEGNUM AND DEGREE.DEGREE=DEGNAME.DEGREE $fn $ln $dg $gd $ct $st";
-	
-	if($r1 = mysql_query($query) && $x==0){
-		echo '<table class="table table-striped table-hover ">';
-        echo '<thead>';
-        echo '<tr class="info">';
-        echo '<th>Select</th>';
-        echo '<th>First Name</th>';
-        echo '<th>Last Name</th>';
-        echo '<th>Degree</th>';
-        echo '<th>Graduation</th>';
-        echo '<th>City</th>';
-        echo '<th>State</th>';
-        echo '</tr>';
-        echo '</thead>';
-        echo '<tbody>';
-        echo '<tr>';
-       
-		while($row=mysql_fetch_array($r1)){
-			$fname2 = $row['FNAME'];
-			$lname2 = $row['LNAME'];
-			$degree2 = $row['DEGNAME'];
-			$grad2 = $row['GRADDATE'];
-			$city2 = $row['CITY'];
-			$state2 = $row['STATE'];
-			$accnum = $row['ACCOUNTNUM'];
-			?>
-			<div class="box">
-				<form action="#popup1" method="get">
-				<td> <button class="button3" name="var" type="submit" value="<? echo $accnum ?>">View</button></td>
-			</div>
-			<?php
-			//?var=<?php echo $accnum
-        	echo '<td>'.$fname2.'</td>';
-            echo '<td>'.$lname2.'</td>';
-            echo '<td>'.$degree2.'</td>';
-            echo '<td>'.$grad2.'</td>';
-            echo '<td>'.$city2.'</td>';
-            echo '<td>'.$state2.'</td>';
-            echo '</div>';
-            echo '</tr>';
-                                
+	if ($x==0){
+		if($r1 = mysql_query($query)){
+			echo '<table class="table table-striped table-hover ">';
+	        echo '<thead>';
+	        echo '<tr class="info">';
+	        echo '<th>Select</th>';
+	        echo '<th>First Name</th>';
+	        echo '<th>Last Name</th>';
+	        echo '<th>Degree</th>';
+	        echo '<th>Graduation</th>';
+	        echo '<th>City</th>';
+	        echo '<th>State</th>';
+	        echo '</tr>';
+	        echo '</thead>';
+	        echo '<tbody>';
+	        echo '<tr>';
+	       
+			while($row=mysql_fetch_array($r1)){
+				$fname2 = $row['FNAME'];
+				$lname2 = $row['LNAME'];
+				$degree2 = $row['DEGNAME'];
+				$grad2 = $row['GRADDATE'];
+				$city2 = $row['CITY'];
+				$state2 = $row['STATE'];
+				$accnum = $row['ACCOUNTNUM'];
+				?>
+				<div class="box">
+					<form action="#popup1" method="get">
+					<td> <button class="button3" name="var" type="submit" value="<? echo $accnum ?>">View</button></td>
+				</div>
+				<?php
+				//?var=<?php echo $accnum
+	        	echo '<td>'.$fname2.'</td>';
+	            echo '<td>'.$lname2.'</td>';
+	            echo '<td>'.$degree2.'</td>';
+	            echo '<td>'.$grad2.'</td>';
+	            echo '<td>'.$city2.'</td>';
+	            echo '<td>'.$state2.'</td>';
+	            echo '</div>';
+	            echo '</tr>';
+	                                
+			}
+			echo '</tbody>';
+	      	echo '</table>';
+	      	
 		}
-		echo '</tbody>';
-      	echo '</table>';
-      	
 	}
 	
 ?>
