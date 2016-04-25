6b9426d7570b33b43940239b4ae3784fd0d4f07c
jpowell2 <Jeremy@Jeremys-MacBook-Pro.local>
2016-04-25 18:27:35 -0400

    butts

--- browse_display.php
+++ browse_display.php
@@ -2,13 +2,10 @@
 <div id="popup1" class="overlay3">
 	<?php
 	    $var = $_GET['var'];
-<<<<<<< Updated upstream
+
 		session_start();
 		$_SESSION['toser'] = $var;
-		
-=======
-		//session_start();
->>>>>>> Stashed changes
+	
 		include "dataLogin.php";
 		$i = $_SESSION['email'];
 		
